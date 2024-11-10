02455376693600dba87c5f47456ba6fdc383f6ca
Begic Faris <farisbegic9@gmail.com>
2024-11-10 23:20:40 +0100

    End of section 3

--- Api/Program.cs
+++ Api/Program.cs
@@ -7,14 +7,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddDbContext<DataContext>(opt => {
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+builder.Services.AddDbContext<DataContext>(opt =>
+ {
+     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 
-});
+ });
+builder.Services.AddCors(); 
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(x=> x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200","https://localhost:4200"));
+
 app.MapControllers();
 
 app.Run();
